8d74c2b2f307ecbc7ce2c5ec6aeb6c745ae83ba6
seonghun <pch468@naver.com>
2023-04-12 23:13:56 +0900

    feat: signin 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp.tsx';
+import SignIn from './pages/SignIn.tsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
     </Routes>
   );
 }
