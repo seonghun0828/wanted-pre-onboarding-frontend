7c0a08ffe1a6e5f366c572c85fa473b5c0e072d7
seonghun <pch468@naver.com>
2023-04-13 15:05:54 +0900

    feat: axios default url 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp.tsx';
 import SignIn from './pages/SignIn.tsx';
+import axios from 'axios';
 
 function App() {
+  axios.defaults.baseURL = 'https://www.pre-onboarding-selection-task.shop';
   return (
     <Routes>
       <Route path="/signup" element={<SignUp />} />
