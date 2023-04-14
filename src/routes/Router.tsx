67c9f9e139dfc7e7105241348e8bf5785436ff5f
seonghun <pch468@naver.com>
2023-04-14 15:50:51 +0900

    feat: 로그인 유무에 따라 자동으로 라우팅 되도록 PrivateRoute 설정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from '../pages/SignUp.tsx';
 import SignIn from '../pages/SignIn.tsx';
+import PrivateRoute from './PrivateRoute.tsx';
+import Todo from '../pages/Todo.tsx';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route element={<PrivateRoute authentication={false} />}>
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+        <Route element={<PrivateRoute authentication={false} />}>
+          <Route path="/signin" element={<SignIn />} />
+        </Route>
+        <Route element={<PrivateRoute authentication={true} />}>
+          <Route path="/todo" element={<Todo />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
