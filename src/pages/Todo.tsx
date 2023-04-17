3df1bc856f92150558bc1e977539946d7a014d11
seonghun <pch468@naver.com>
2023-04-17 14:40:50 +0900

    feat: createTodo 파일 및 로직 작성

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { createTodo } from '../axios_fns/createTodo.ts';
 
 interface TodoType {
   id: number;
@@ -9,6 +10,16 @@ interface TodoType {
 
 const Todo = () => {
   const [todos, setTodos] = useState<TodoType[]>([]);
+  const [input, setInput] = useState('');
+
+  const clickHandler = () => {
+    createTodo(input).then((todo: TodoType) => {
+      setTodos((prev) => [...prev, todo]);
+    });
+  };
+  const changeTextHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
   return (
     <div className="flex justify-center w-full p-10">
       <div className="flex flex-col justify-center w-1/2 gap-2">
@@ -18,17 +29,23 @@ const Todo = () => {
             <input
               className="border"
               type="text"
+              value={input}
+              onChange={(e) => changeTextHandler(e)}
               data-testid="new-todo-input"
             />
-            <button className="border" data-testid="new-todo-add-button">
+            <button
+              className="border"
+              onClick={clickHandler}
+              data-testid="new-todo-add-button"
+            >
               추가
             </button>
           </div>
           <ul className="flex flex-col">
             {todos.map(({ id, todo, isCompleted }) => (
-              <li>
+              <li key={id}>
                 <label className="flex gap-2" htmlFor="">
-                  <input type="checkbox" />
+                  <input type="checkbox" checked={isCompleted} />
                   <span>{todo}</span>
                 </label>
               </li>
