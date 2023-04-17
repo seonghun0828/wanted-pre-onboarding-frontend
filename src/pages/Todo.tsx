aff24debf21693819e4bad05bd84b208fff3d7b7
seonghun <pch468@naver.com>
2023-04-17 10:22:20 +0900

    feat: Todo 페이지 기본 템플릿 작성

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface TodoType {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
+}
 
 const Todo = () => {
-  return <div>Todo Page</div>;
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  return (
+    <div className="flex justify-center w-full p-10">
+      <div className="flex flex-col justify-center w-1/2 gap-2">
+        <div className="flex flex-col">
+          <h1 className="text-2xl text-center">To do</h1>
+          <div className="flex justify-center gap-2">
+            <input
+              className="border"
+              type="text"
+              data-testid="new-todo-input"
+            />
+            <button className="border" data-testid="new-todo-add-button">
+              추가
+            </button>
+          </div>
+          <ul className="flex flex-col">
+            {todos.map(({ id, todo, isCompleted }) => (
+              <li>
+                <label className="flex gap-2" htmlFor="">
+                  <input type="checkbox" />
+                  <span>{todo}</span>
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
